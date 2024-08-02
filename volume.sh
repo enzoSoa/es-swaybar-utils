608a3af91c1136cd3d0e42e0bb44713564579a6b
enzoSoa <enzoayrsoares@gmail.com>
2024-08-02 19:11:32 +0200

    Adding headphone icon in volume script

--- volume.sh
+++ volume.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+is_plugged=$(amixer -c0 get Headphone | grep "Front Left" | grep "\[on\]" -o) 
+
 volume_not_trimmed=$(amixer get Master | grep "Front Left" | grep "[0-9]*%" -o) 
 volume=${volume_not_trimmed//%}
 
-if (($volume > "50")); then
+if [[ "$is_plugged" == "[on]" ]]; then
+  icon="";
+elif (($volume > "50")); then
   icon="";
 elif (($volume > "0")); then
   icon="";
